ab80cced2d352406c51ef4be7eec5c5668ccd3e4
Bryan Hadinata <bryanhadinata76@gmail.com>
2024-04-16 22:07:56 +0700

    feat: add dockerfile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Todo from '@/components/Todo'
 import TodoForm from '@/components/TodoForm'
 import { getAllTodos } from '@/lib/actions'
+import { Todo as TodoProps } from '@prisma/client'
 import React from 'react'
 
 async function HomePage() {
@@ -11,7 +12,7 @@ async function HomePage() {
             <TodoForm/>
             <div className="border p-4 rounded-md flex-1">
               <div className='flex flex-wrap gap-2'>
-                {todos.map((todo) => (
+                {todos.map((todo: TodoProps) => (
                     <Todo todo={todo} key={todo.id} />
                 ))}
 
